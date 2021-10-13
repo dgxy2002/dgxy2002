203f8ee87cf9aa88d9c5beb66dcd7793c5b15519
Darren Goh <92384554+dgxy2002@users.noreply.github.com>
2021-10-13 13:28:20 +0800

    Add files via upload

---
+++ guessinggame_v1.0.py
@@ -0,0 +1,23 @@
+#last updated 13102021
+
+print("Welcome to my word guessing game!")
+
+#for game master to enter the secret word
+word = input("Key in your secret word here!: ")
+
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter the word!: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("You ran out of guesses!")
+else:
+    print("You got it!")
